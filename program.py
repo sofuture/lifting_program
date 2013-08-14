f525bf69f8fce08b39b2243a554e1cfaaf6d13cc
jz <jeff.zellner@gmail.com>
2013-08-14 13:03:36 -0400

    changes

--- program.py
+++ program.py
@@ -4,13 +4,26 @@ import os
 
 base_dir = os.path.join(os.curdir, 'data')
 
+goals = {
+    'Snatch': 60,
+    'CJ': 85,
+    'BSQ': 150,
+    'FSQ': 100,
+}
+
 def get_weeks():
     return [d for d in os.listdir(base_dir)
             if os.path.isdir(os.path.join(base_dir, d))]
 
 def get_days(week):
-    return [d for d in os.listdir(os.path.join(base_dir, week))]
+    return [os.path.join(base_dir, week, d) for d in os.listdir(os.path.join(base_dir, week))]
+
+def print_day(d):
+    with open(d) as f:
+        dat = f.read()
+    print dat
 
 if __name__ == "__main__":
     for week in get_weeks():
-        print get_days(week)
+        for d in get_days(week):
+            print_day(d)
